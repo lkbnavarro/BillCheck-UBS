51acc17e760db4be3247f566b427f60e41e67cd3
Lance Navarro <lancegbn@gmail.com>
2023-01-26 16:27:19 +0800

    changes

--- assets/js/xlsx-manip.js
+++ assets/js/xlsx-manip.js
@@ -24,50 +24,52 @@ function openFileDialog(fileNumber) {
 function pushFile(fileNumber, filePath) {
   let fileFound = files.find((file) => file?.id == fileNumber)
   if (fileFound == undefined) {
-      files.push({
-          id: fileNumber,
-          path: filePath
-      })
+    files.push({
+      id: fileNumber,
+      path: filePath
+    })
   } else {
-      let index = files.findIndex((file) => file.fileNumber == fileFound.fileNumber)
-      files[index] = {
-          id: fileNumber,
-          path: filePath
-      }
+    let index = files.findIndex((file) => file.fileNumber == fileFound.fileNumber)
+    files[index] = {
+      id: fileNumber,
+      path: filePath
+    }
   }
 }
 
 function getFileWorksheetJson(fileId, sheetName) {
   let excelFile = files.find((file) => file?.id == fileId)
-  const workbook = xlsx.readFile(excelFile.path, { cellDates:true });
+  const workbook = xlsx.readFile(excelFile.path, { cellDates: true });
   const worksheet = workbook.Sheets[sheetName];
- 
+
   return xlsx.utils.sheet_to_json(worksheet)
 }
 
-function getServiceBillingJson(){
+function getServiceBillingJson() {
   // Read Excel Files for SBR
   // Get data that has number on column 1
   return getFileWorksheetJson("file-1", "Resource Reporting - MC")
-  .filter((item) => typeof item['__EMPTY_1'] === 'number')
+    .filter((item) => typeof item['__EMPTY_1'] === 'number')
 };
 
-function getInnovationBillingJson(){
+function getInnovationBillingJson() {
   // Read Excel Files for Resource List
-  return getFileWorksheetJson("file-2", "myTE Innov bookings")
+  const innovationJson = getFileWorksheetJson("file-2", "myTE Innov bookings")
+  const totalRowIndex = innovationJson.findIndex((row) => row['Contract Type'].toUpperCase() == 'TOTAL')
+  return innovationJson.slice(0, totalRowIndex)
 };
 
-function getResourceListJson(){
+function getResourceListJson() {
   // Read Excel Files for Resource List
   return getFileWorksheetJson("file-3", "UBS Staff List")
 };
 
-function getResourceTrendJson(){
+function getResourceTrendJson() {
   // Read Excel Files for Resource Trend
   return getFileWorksheetJson("file-4", "Raw Data")
 };
 
-function getPWAJson(){
+function getPWAJson() {
   // Read Excel Files for Resource List
   return getFileWorksheetJson("file-5", "PWAInput")
 };
@@ -155,7 +157,7 @@ function createRowsFromSBR(sbrFileJson, resourceListJson, resourceTrendJson) {
   })
 }
 
-function createRowsFromPwa(pwaJson,resourceTrendJson) {
+function createRowsFromPwa(pwaJson, resourceTrendJson) {
   return pwaJson.map((data) => {
 
     let stream = data['Segment']
@@ -209,10 +211,10 @@ function createRowsFromPwa(pwaJson,resourceTrendJson) {
 
 
 
-function createRowsFromInnovationBilling(innovationBillingJson,resourceTrendJson) {
+function createRowsFromInnovationBilling(innovationBillingJson, resourceTrendJson) {
   return innovationBillingJson.map((data) => {
     let stream = data['STREAM']
-    let enterpriseId = ['Enterprise ID']
+    let enterpriseId = data['Enterprise ID']
     let locationCategory = data['Location']
     let dailyRate = data['GROSS daily rate']
     let billableHoursA = data["Hrs\r\n(based on myTE)_1"]
@@ -261,7 +263,6 @@ function createRowsFromInnovationBilling(innovationBillingJson,resourceTrendJson
 }
 
 
-
 function getEnterpriseId(fullName, resourceListJson) {
   let resourceItem = resourceListJson.find((item) => item["Full Name"] == fullName)
   return resourceItem?.['Enterprise ID'] || "N/A"
@@ -281,7 +282,7 @@ function getStream(stream) {
   return (stream == 'PAY & MANAGE LIQUIDITY') ? 'INNOVATION INITIATIVES' : stream
 }
 
-function calculateDailyRate(onshoreRate, offshoreRate){
+function calculateDailyRate(onshoreRate, offshoreRate) {
   return (onshoreRate + offshoreRate)
 }
 
@@ -318,62 +319,62 @@ function calculatestrategicInnovFundB(grossAmountB, volDiscountB) {
 }
 
 function calculateNetAmountA(grossAmountA, volDiscountA, strategicInnovFundA) {
-  return (grossAmountA+volDiscountA+strategicInnovFundA)
+  return (grossAmountA + volDiscountA + strategicInnovFundA)
 }
 
 function calculateNetAmountB(grossAmountB, volDiscountB, strategicInnovFundB) {
-  return (grossAmountB+volDiscountB+strategicInnovFundB)
+  return (grossAmountB + volDiscountB + strategicInnovFundB)
 }
 
 function calculateBillDaysVar(billableDaysB, billableDaysA) {
-  return Math.abs(billableDaysA-billableDaysB)
+  return Math.abs(billableDaysA - billableDaysB)
 }
 
 function calculateNetAmountVar(netAmountB, netAmountA) {
-  return (netAmountA-netAmountB)
+  return (netAmountA - netAmountB)
 }
 
-function getBillDaysMME(locationCategory, billableHoursB, fullName){
+function getBillDaysMME(locationCategory, billableHoursB, fullName) {
   let result = 0
-  if (fullName == 'Largados, Mickie Rose'){
+  if (fullName == 'Largados, Mickie Rose') {
     result = billableHoursB / 8
-  } else if (fullName == 'Garcia, Julius Armstrong'){
+  } else if (fullName == 'Garcia, Julius Armstrong') {
     result = billableHoursB / 9
   } else {
-    if (locationCategory == 'Philippines Offshore'){
+    if (locationCategory == 'Philippines Offshore') {
       result = billableHoursB / 9
-    } else if (locationCategory == 'Switzerland Local'){
+    } else if (locationCategory == 'Switzerland Local') {
       result = billableHoursB / 8
-    } else if (locationCategory == 'Philippines Landed in CH'){
+    } else if (locationCategory == 'Philippines Landed in CH') {
       result = billableHoursB / 9
-    } else if (locationCategory == 'Philippines Landed in SG'){
+    } else if (locationCategory == 'Philippines Landed in SG') {
       result = billableHoursB / 8
-    } else if (locationCategory == 'Singapore Local'){
+    } else if (locationCategory == 'Singapore Local') {
       result = billableHoursB / 8
-    } else if (locationCategory == 'India Local'){
+    } else if (locationCategory == 'India Local') {
       result = billableHoursB / 9
-    } else if (locationCategory == 'Switzerland - Manno Local'){
+    } else if (locationCategory == 'Switzerland - Manno Local') {
       result = billableHoursB / 8
-    } else if (locationCategory == 'Philippines Landed in HK'){
+    } else if (locationCategory == 'Philippines Landed in HK') {
       result = billableHoursB / 7.5
-    } else if (locationCategory == 'Hong Kong Local'){
+    } else if (locationCategory == 'Hong Kong Local') {
       result = billableHoursB / 7.5
-    } else if (locationCategory == 'India Landed in SG'){
+    } else if (locationCategory == 'India Landed in SG') {
       result = billableHoursB / 8
     } else {
       result
-      }
     }
-  return (result)
   }
+  return (result)
+}
 
-function getBillDaysPWA(locationCategory, billableHoursB){
+function getBillDaysPWA(locationCategory, billableHoursB) {
   let result = 0
-  if (locationCategory == 'Landed - CH'){
+  if (locationCategory == 'Landed - CH') {
     result = billableHoursB / 9
-  } else if (locationCategory == 'Local'){
-    result = billableHoursB / 8 
-  } else if (locationCategory == 'Offshore'){
+  } else if (locationCategory == 'Local') {
+    result = billableHoursB / 8
+  } else if (locationCategory == 'Offshore') {
     result = billableHoursB / 9
   } else {
     result
@@ -381,200 +382,65 @@ function getBillDaysPWA(locationCategory, billableHoursB){
   return (result)
 }
 
-function calculateBillDaysAInnovationBilling(billableHoursA, hrsperday){
+function calculateBillDaysAInnovationBilling(billableHoursA, hrsperday) {
   return (billableHoursA / hrsperday)
 }
 
-function createExcel() {
-    let serviceBillingJson = getServiceBillingJson();
-    let innovationBillingJson = getInnovationBillingJson();
-    let resourceListJson = getResourceListJson();
-    let resourceTrendJson = getResourceTrendJson();
-    let pwaJson = getPWAJson();
-
-    // console.log(serviceBillingJson)
-    // console.log(innovationBillingJson)
-    // console.log(resourceListJson)
-    // console.log(resourceTrendJson)
-    // console.log(pwaJson)
-    
-    let sbrRows = createRowsFromSBR(serviceBillingJson, resourceListJson, resourceTrendJson)
-    let pwaRows = createRowsFromPwa(pwaJson, resourceTrendJson)
-    let innovationRows = createRowsFromInnovationBilling(innovationBillingJson, resourceTrendJson)
-    let mergedRows = [...sbrRows, ...pwaRows,...innovationRows]
-
-
-    var workbook = xlsx.utils.book_new();
-    var worksheet = xlsx.utils.json_to_sheet(mergedRows);
-    worksheet["!cols"] = [ { wch: 40 }, {wch: 20}, {wch:23}, {wch:12}, {wch:22}, {wch:19}, {wch:13}, {wch:18}, {wch:17}, {wch:25}, {wch:25}, 
-      {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}, {wch:25}];
-    xlsx.utils.book_append_sheet(workbook, worksheet, "testSheet");
-    xlsx.writeFile(workbook, "textBook.xlsx");
+function distinct(iterable) {
+  let set = new Set();
+  return iterable.filter(item => {
+    let value = JSON.stringify(item);
+    if (set.has(value)) {
+      return false;
+    } else {
+      set.add(value);
+      return true;
+    }
+  });
 }
 
+function filterByIdStreamRole(rows) {
+  return rows.filter((item, index) => rows.findIndex(x =>
+    x['Enterprise ID'] === item['Enterprise ID'] && x['STREAM'] === item['STREAM']
+    && x['Hermes Role'] === item['Hermes Role']) === index)
+}
 
+function roundToTwoDecimal(number) {
+  return parseFloat(number.toFixed(2))
+}
 
-// function pushSBRtoArray(range, worksheetSBR, streamValues, resourceName, hermesRole, hermesLevel, locationCat, dailyrateOn, dailyrateOff, sbrBillHrs, sbrBillDays){
-//   for (let rowNum = 13; rowNum <= range.e.r; rowNum++) {
-//     // Get the cell value
-//     let streamCell = worksheetSBR["C" + rowNum];
-//     if (streamCell) {
-//       streamValues.push(streamCell.v);
-//     }
-//     let resourceNameCell = worksheetSBR["G"+rowNum];
-//     if (resourceNameCell){
-//       resourceName.push(resourceNameCell.v);
-//     }
-//     let hermesRoleCell = worksheetSBR["H" + rowNum];
-//     if (hermesRoleCell) {
-//       hermesRole.push(hermesRoleCell.v);
-//     }
-//     let hermesLevelCell = worksheetSBR["I" + rowNum];
-//     if (hermesLevelCell) {
-//       hermesLevel.push(hermesLevelCell.v);
-//     }
-//     let locationCatCell = worksheetSBR["J" + rowNum];
-//     if (locationCatCell) {
-//       locationCat.push(locationCatCell.v);
-//     }
-  
-//     let dailyrateOnCell = worksheetSBR["N" + rowNum];
-//     if (dailyrateOnCell) {
-//       dailyrateOn.push(dailyrateOnCell.v);
-//     }
-//     let dailyrateOffCell = worksheetSBR["O" + rowNum];
-//     if (dailyrateOffCell) {
-//       dailyrateOff.push(dailyrateOffCell.v);
-//     }
-//     let sbrBillHrsCell = worksheetSBR["AG" + rowNum];
-//     if (sbrBillHrsCell) {
-//       sbrBillHrs.push(sbrBillHrsCell.v);
-//     }
-//     let sbrBillDaysCell = worksheetSBR["AH" + rowNum];
-//     if (sbrBillDaysCell) {
-//       sbrBillDays.push(sbrBillDaysCell.v);
-//     }
-//   }
-// };
+function formatRows(array) {
+  array.forEach(item => {
+    item['Net Amount C'] = roundToTwoDecimal(item['Net Amount C'])
+    item['Billable Days C'] = roundToTwoDecimal(item['Billable Days C'])
+  })
+}
 
-// function pushRLtoArray(range, worksheetRL){
-//   for (let rowNum = 2; rowNum <= range.e.r; rowNum++) {
-//     let resourceFullNameCell = worksheetRL["B" + rowNum];
-//     if (resourceFullNameCell) {
-//     resourceFullName.push(resourceFullNameCell.v);
-//     }
-//     let enterpriseIDCell = worksheetRL["C"+rowNum];
-//     if (enterpriseIDCell){
-//     enterpriseID.push(enterpriseIDCell.v);
-//     }
-//   }
-// };
+function createExcel() {
+  let serviceBillingJson = getServiceBillingJson();
+  let innovationBillingJson = getInnovationBillingJson();
+  let resourceListJson = getResourceListJson();
+  let resourceTrendJson = getResourceTrendJson();
+  let pwaJson = getPWAJson();
 
-// function replaceStream(){
-//   const oldStream = 'PAY & MANAGE LIQUIDITY';
-//   const newStream = 'INNOVATION INITIATIVES';
-//   for (let i = 0; i < streamValues.length; i++){
-//     if(streamValues[i] === oldStream){
-//       streamValues.splice(i, 1, newStream);
-//     }
-//   }
-// };
+  // console.log(serviceBillingJson)
+  // console.log(innovationBillingJson)
+  // console.log(resourceListJson)
+  // console.log(resourceTrendJson)
+  // console.log(pwaJson)
 
-// function calculateGross(){
-//     let grossAmountOn = [];
-//     let grossAmountOff = [];
-//     let grossAmount = [];
+  let sbrRows = createRowsFromSBR(serviceBillingJson, resourceListJson, resourceTrendJson)
+  let innovationRows = createRowsFromInnovationBilling(innovationBillingJson, resourceTrendJson)
+  let pwaRows = createRowsFromPwa(pwaJson, resourceTrendJson)
+  let mergedDistinctRows = distinct([...sbrRows, ...pwaRows, ...innovationRows])
+  let finalArray = filterByIdStreamRole(mergedDistinctRows)
+  formatRows(finalArray)
 
-//     for (let i = 0; i < array1.length; i++){
-//         sumArray[i] = array1[i] + array2[i];
-//     }  
-// }
+  var workbook = xlsx.utils.book_new();
+  var worksheet = xlsx.utils.json_to_sheet(finalArray);
+  worksheet["!cols"] = [{ wch: 40 }, { wch: 20 }, { wch: 23 }, { wch: 12 }, { wch: 22 }, { wch: 19 }, { wch: 13 }, { wch: 18 }, { wch: 17 }, { wch: 25 }, { wch: 25 },
+  { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 25 }];
 
-
-// // For Gross Amount
-
-// /* let grossAmount = [];
-
-// for (let i = 0; i < sbrBillDays.length; i++){
-//   grossAmount.push(sbrBillDays[i] * 
-// }
-// */
-
-// /* COMPARING ARRAYS ANG GETTING THEIR ENTERPRISE ID
-
-// const newResourceName = resourceName.map(item => item.trim());
-// const newResourceFullName = resourceFullName.map(item => item.trim());
-
-// let count = 0;
-
-// for (let i = 0; i < newResourceName.length; i++) {
-//   let flag = true;
-//   for (let j = 0; j < newResourceFullName.length; j++) {
-//       if (newResourceName[i] === newResourceFullName[j]) {
-//           flag = false;
-//           break;
-//       }
-//   }
-//   if (flag) {
-//       console.log(newResourceName[i]);
-//   }
-// }
-
-
-// for (let i = 0; i < newResourceFullName.length; i++) {
-//   let flag = true;
-//   for (let j = 0; j < newResourceName.length; j++) {
-//       if (newResourceFullName[i] === newResourceName[j]) {
-//           flag = false;
-//           break;
-//       }
-//   }
-//   if (flag) {
-//       console.log(newResourceFullName[i]);
-//   }
-// }
-
-// */
-
-// function pushValues(){
-//     const newWorkbook = xlsx.utils.book_new();
-//     let outputData = [];
-//     outputData.push(outputHeader);
-
-//     for(let i=0; i<streamValues.length; i++) {
-//       outputData.push([streamValues[i], enterpriseID[i], hermesRole[i], hermesLevel[i], locationCat[i], dailyrateOn[i], dailyrateOff[i], sbrBillHrs[i], 
-//         sbrBillDays[i]])
-//     };
-// };
-
-
-// function writeFile(){
-//     const newWorksheet = xlsx.utils.aoa_to_sheet(outputData);
-//     newWorksheet["!cols"] = [ { wch: 40 }, {wch: 30}, {wch:23}, {wch:12}, {wch:22}, {wch:19}, {wch:19}, {wch:18}, {wch:17}, {wch:30}, {wch:30}, 
-//       {wch:30}, {wch:30}, {wch:30}, {wch:30}, {wch:30}, {wch:30}];
-    
-//     // Add the new worksheet to the new workbook
-//     xlsx.utils.book_append_sheet(newWorkbook, newWorksheet, 'Sheet 1');
-    
-//     // Write the new workbook to a file
-//     xlsx.writeFile(newWorkbook, 'new-test.xlsx');
-// };
-
-// function testOutput(){
-//     console.log(sbrBillDays);
-//     console.log(sbrBillHrs);
-//     console.log(dailyrateOff); 
-//   };
-
-
-// function testExecute(){
-//     readSBR();
-//     readRL();
-//     createArray();
-//     pushSBRtoArray();
-//     pushRLtoArray();
-//     replaceStream();
-//     testOutput();
-// };
-
-// testExecute();
+  xlsx.utils.book_append_sheet(workbook, worksheet, "testSheet");
+  xlsx.writeFile(workbook, "textBook.xlsx");
+}
